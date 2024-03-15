390dec3cb75d57870649ea08a75d5b5f87032121
Rodrigueseduardo26 <duduropaz@gmail.com>
2024-03-15 08:24:09 -0300

    variavel nome

--- scripts.js
+++ scripts.js
@@ -1,5 +1,6 @@
 var botao = document.getElementById("botao");
 botao.addEventListener("click", alertar, false);
+var nome = document.getElementById("nome");
 
 function alertar(event){
     alert("você clicou no botão!!!" + event);
